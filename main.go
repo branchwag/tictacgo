e68051bc4dcbd43f87ed7db74a488f03cb78903a
LeAnne B. <branchleannelmb@gmail.com>
2024-05-23 19:05:13 -0400

    allows user to put X or O in square

--- main.go
+++ main.go
@@ -2,87 +2,53 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
-	"sync"
 )
 
-var tmpl = template.Must(template.ParseFiles("index.html"))
-var mu sync.Mutex
 
 type Game struct {
-	Board [3][3]string
+	Board [][]string
 	Player string
 	Winner string
 }
 
 var game = Game{
-	Board: [3][3]string{{"","",""}, {"","",""}, {"","",""}},
+	Board: [][]string{
+		{"","",""},
+		{"","",""},
+		{"","",""},
+	},
 	Player: "X",
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
-	mu.Lock()
-	defer mu.Unlock()
-	tmpl.Execute(w, game)
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, game)
 }
 
 func moveHandler(w http.ResponseWriter, r *http.Request){
-	mu.Lock()
-	defer mu.Unlock()
-
-	if game.Winner != "" {
-		tmpl.Execute(w, game)
-		return
-	}
-
-	row, err1 := strconv.Atoi(r.URL.Query().Get("row"))
-	col, err2 := strconv.Atoi(r.URL.Query().Get("col"))
-
-	if err1 != nil || err2 != nil || row < 0 || row > 2 || col < 0 || col > 2{
-		tmpl.Execute(w, game)
-		return
-	}
+	rowStr := r.URL.Query().Get("row")
+	colStr := r.URL.Query().Get("col")
+	row, _ := strconv.Atoi(rowStr)
+	col, _ := strconv.Atoi(colStr)
 
 	if game.Board[row][col] == "" {
 		game.Board[row][col] = game.Player
-		if checkWinner(game.Board, game.Player){
-			game.Winner = game.Player
+		if game.Player == "X" {
+			game.Player = "O"
 		} else {
-			if game.Player == "X" {
-				game.Player = "O"
-			} else {
-				game.Player = "X"
-			}
+			game.Player="X"
 		}
 	}
-
-	tmpl.Execute(w, game)
+	w.Write([]byte(game.Board[row][col]))
 }
 
-func checkWinner(board [3][3]string, player string) bool {
-	for i := 0; i < 3; i++ {
-		if board[i][0] == player && board[i][1] == player && board[i][2] == player {
-			return true
-		}
-		if board[0][i] == player && board[1][i] == player && board[2][i] == player {
-			return true
-		}
-	}
-	if board[0][0] == player && board[1][1] == player && board[2][2] == player {
-		return true
-	}
-	if board[0][2] == player && board[1][1] == player && board[2][0] ==player {
-		return true
-	}
-	return false
-}
 
 func main(){
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/move", moveHandler)
-	http.Handle("static/", http.StripPrefix("/static", http.FileServer(http.Dir("."))))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+	http.ListenAndServe(":8080", nil)
 }
 
